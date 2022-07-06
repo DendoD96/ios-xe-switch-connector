20c676ff78a79638184d493f0c75e89ac28e5af6
Daniele Rossi <daniele.rossi@vem.com>
2022-07-06 11:53:32 +0200

    Modify API

--- ios_xe_switch_connector/controllers/default_controller.py
+++ ios_xe_switch_connector/controllers/default_controller.py
@@ -5,13 +5,29 @@ import requests
 from ios_xe_switch_connector import util
 import os
 from dotenv import load_dotenv
+from ios_xe_switch_connector.models.vlan_info import VlanInfo  # noqa: E501
 
 load_dotenv()
 
-def say_hello():
-    """say hello
+def create_vlan(body):  # noqa: E501
+    """create vlan
 
-    Simple API that hails.
+    Create a vlan for a device interface  # noqa: E501
+
+    :param body: 
+    :type body: dict | bytes
+
+    :rtype: str
+    """
+    if connexion.request.is_json:
+        body = VlanInfo.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def get_vlan():  # noqa: E501
+    """get vlans
+
+    Get all vlans on a switch.  # noqa: E501
 
 
     :rtype: str
