4e3e31066c51939a80c38db6eae2338bb8f51c5f
Emile <lalouoti.nitcheu@studio.unibo.it>
2022-07-08 01:49:58 -0700

    changes done in order to allow a patch still  not working

--- ios_xe_switch_connector/controllers/default_controller.py
+++ ios_xe_switch_connector/controllers/default_controller.py
@@ -1,4 +1,6 @@
 from asyncio.log import logger
+import json
+import re
 import connexion
 import six
 import requests
@@ -8,6 +10,7 @@ from dotenv import load_dotenv
 from ios_xe_switch_connector.models.vlan_info import VlanInfo  # noqa: E501
 
 load_dotenv()
+
 
 def create_vlan(body):  # noqa: E501
     """create vlan
@@ -19,9 +22,32 @@ def create_vlan(body):  # noqa: E501
 
     :rtype: str
     """
+
+    hst = os.getenv('HOST')
     if connexion.request.is_json:
         body = VlanInfo.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        name = body.interface_name
+        vlan = body.vlan_number
+        
+        regex = r'^([^\d]+)(\d+.*)$'
+        splitted_iface = re.search(regex, name)
+
+        body_a = {
+            "Cisco-IOS-XE-native:TwoGigabitEthernet": [
+                {
+                    "name": splitted_iface[2],
+                    "switchport": {
+                        "Cisco-IOS-XE-switch:access": {
+                            "vlan": {
+                                "vlan": vlan
+                            }
+                        }
+                    }}]}
+        api_url = f'https://{hst}:443/restconf/data/Cisco-IOS-XE-native:native/interface/{splitted_iface[1]}'
+        headers = {'Accept': 'application/yang-data+json', 'Content-Type': 'application/yang-data+json'}
+        response = requests.patch(api_url, verify=False, headers=headers,
+                                  auth=(os.getenv('USRNAME'), os.getenv('PASSWORD')), data=json.dumps(body_a))
+        return response.json()
 
 
 def get_vlan():  # noqa: E501
@@ -33,7 +59,8 @@ def get_vlan():  # noqa: E501
     :rtype: str
     """
     hst = os.getenv('HOST')
-    headers = {'Accept':'application/yang-data+json'}
+    headers = {'Accept': 'application/yang-data+json'}
     api_url = f'https://{hst}:443/restconf/data/Cisco-IOS-XE-vlan-oper:vlans/vlan'
-    response = requests.get(api_url, verify = False, headers = headers, auth = (os.getenv('USRNAME'), os.getenv('PASSWORD')))
+    response = requests.get(api_url, verify=False, headers=headers, auth=(
+        os.getenv('USRNAME'), os.getenv('PASSWORD')))
     return response.json()
